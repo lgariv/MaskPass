aada6810048c1c721f61a165aa761dcfba560423
Lavie <lavie43b@gmail.com>
2021-01-31 12:00:53 -0800

    Update install-requirements.sh

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd models/research
 protoc object_detection/protos/*.proto --python_out=.
+cp object_detection/packages/tf2/setup.py .
 python setup.py build
 python setup.py install
 cd ../..
